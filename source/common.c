2066b9a759efc706a218dff452833da4613572d0
Vegard Sjonfjell <vsjonfje@cisco.com>
2021-11-20 23:55:23 +0100

    Removed annoying printf

--- source/common.c
+++ source/common.c
@@ -1393,7 +1393,6 @@ int COM_FindFile (char *filename, int *handle, FILE **file)
 			for (i=0 ; i<pak->numfiles ; i++)
 				if (!strcmp (pak->files[i].name, filename))
 				{       // found it!
-					Sys_Printf ("PackFile: %s : %s\n",pak->filename, filename);
 					if (handle)
 					{
 						*handle = pak->handle;
